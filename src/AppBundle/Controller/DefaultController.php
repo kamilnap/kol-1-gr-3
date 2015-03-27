e327daf2e41ada080f5abd8025775212da4e2be4
Pawel Ciepielewski <pawelc1993@gmail.com>
2015-03-27 09:48:32 +0100

    Zrób komu dobrze

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -46,7 +46,13 @@ class DefaultController extends Controller
     /* N */
 
     /* O */
-
+    /**
+ * @Route("/zrob-komu-dobrze.html", name="zrob-komu-dobrze")
+ */
+public function  zrobKomuDobrzeAction()
+{
+    return $this->render('default/zrob-komu-dobrze.html.twig');
+}
     /* P */
 
     /* Q */
